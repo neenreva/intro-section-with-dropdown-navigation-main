f11f2b4bc7ea7838107c303d839902a5cd60a661
Jimmy G <neenreva@gmail.com>
2022-05-17 11:10:49 -0400

    Rework nav with functionality

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hey there!');
+document.addEventListener("click", (e) => {
+    console.log(e.target.matches('[data-dropdown-btn-mobile]'));
+    console.log(document.querySelector('[data-dropdown-mobile]'));
+
+  if (e.target.matches('[data-dropdown-btn-mobile]')) {
+      document.querySelector('[data-dropdown-mobile]').classList.toggle('active')
+  }
+/* 
+  let currentDropdown;
+  if (isDropdownBtn) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown]").forEach((dropdown) => {
+    if (dropdown == currentDropdown) return;
+    dropdown.classList.remove("active");
+  }); */
+});
