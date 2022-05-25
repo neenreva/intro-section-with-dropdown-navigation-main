6cafc8f9a8275c2f4ea61187d3b6d12490567b68
Jimmy G <neenreva@gmail.com>
2022-05-24 20:36:11 -0400

    Functionality/Layout

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 document.addEventListener("click", (e) => {
-    console.log(e.target.matches('[data-dropdown-btn-mobile]'));
-    console.log(document.querySelector('[data-dropdown-mobile]'));
+  // console.log(e.target.matches("[data-dropdown-btn]"));
+  // console.log(e.target.matches('[data-ham-btn]'));
+  const isHamBtn = e.target.matches("[data-ham-btn]");
+  const isDropdownBtn = e.target.matches("[data-dropdown-btn]");
+  if (!isDropdownBtn && e.target.closest("[data-dropdown]") != null) return;
 
-  if (e.target.matches('[data-dropdown-btn-mobile]')) {
-      document.querySelector('[data-dropdown-mobile]').classList.toggle('active')
-  }
-/* 
   let currentDropdown;
   if (isDropdownBtn) {
+    // console.log(e.target.closest("[data-dropdown]"));
     currentDropdown = e.target.closest("[data-dropdown]");
     currentDropdown.classList.toggle("active");
   }
@@ -15,5 +15,14 @@ document.addEventListener("click", (e) => {
   document.querySelectorAll("[data-dropdown]").forEach((dropdown) => {
     if (dropdown == currentDropdown) return;
     dropdown.classList.remove("active");
-  }); */
+  });
+
+  if (isHamBtn) {
+    let navList = document.querySelector(".nav__list");
+    let menuImg = document.querySelector(".nav__burger");
+    navList.classList.toggle("active");
+    navList.classList.contains("active")
+      ? (menuImg.src = "./images/icon-close-menu.svg")
+      : (menuImg.src = "./images/icon-menu.svg");
+  }
 });
